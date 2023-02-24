ba44f693059bad3bb1a3ed3ca64f3bfdf9436398
Gabriel Araújo <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2023-02-24 17:28:26 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let char = new Knight('Bonieky');
+let monster = new LittleMonster();
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+
+);
+
+stage.start()
